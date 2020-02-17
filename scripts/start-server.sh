0375b6c6d3bd624a62f45957e5635c03a8a76ab2
ich777 <ich777@noreply.example.org>
2020-02-17 18:15:39 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-echo "-----------------------------------------------------"
-echo "------Sending ping every ${PING_INTERVAL} seconds------"
-echo "-------with a timeout of ${PING_TIMEOUT} seconds-------"
-echo "---to: ${HOST}, if the host is not reachable---"
-echo "----it will check again in ${PING_RETRY} seconds----"
-echo "-----------------------------------------------------"
-
-while ping -c 1 -W ${PING_TIMEOUT} "${HOST}" > /dev/null
-do
-        echo "$(date '+%x %X'): ${HOST} is reachable"
-        sleep ${PING_INTERVAL}
-done
-if wget https://api.pushover.net/1/messages.json --post-data="token=${PUSHOVER_APP_TOKEN}&user=${PUSHOVER_USER_TOKEN}&priority=${PUSHOVER_PRIORITY}&title=${PUSHOVER_TITLE}&message=${PUSHOVER_MESSAGE}" -qO- > /dev/null 2>&1 ; then
-        echo "---$(date '+%x %X'): ${HOST} not reachable message sent, retrying after ${PING_RETRY} seconds!---"
-        sleep ${PING_RETRY}
-        /opt/scripts/./$(basename $0) && exit
-else
-        echo "---Can't send message, retrying after ${PING_RETRY} seconds---"
-        sleep ${PING_RETRY}
-        /opt/scripts/./$(basename $0) && exit
-fi
+echo "---Trying to ping: ${HOST} every ${PING_INTERVAL} seconds with a timout of ${PING_TIMEOUT}, retrying if the ping fails after ${PING_RETRY}---"
+/opt/scripts/ping.sh
