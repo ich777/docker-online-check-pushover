888fbb93a5cf4c2f6172ef6d44aaf40192d50b57
ich777 <ich777@noreply.example.org>
2020-02-17 18:37:57 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 echo "---Trying to ping: ${HOST} every ${PING_INTERVAL} seconds with a timout of ${PING_TIMEOUT}, retrying if the ping fails after ${PING_RETRY}---"
-/opt/scripts/ping.sh
+while ping -c 1 -W ${PING_TIMEOUT} "${HOST}" > /dev/null
+do
+	echo "$(date '+%x %X'): ${HOST} is reachable"
+	sleep ${PING_INTERVAL}
+done
+if wget https://api.pushover.net/1/messages.json --post-data="token=${PUSHOVER_APP_TOKEN}&user=${PUSHOVER_USER_TOKEN}&priority=${PUSHOVER_PRIORITY}&title=${PUSHOVER_TITLE}&message=${PUSHOVER_MESSAGE}" -qO- > /dev/null 2>&1 ; then
+	echo "$(date '+%x %X'): ${HOST} not reachable!"
+    echo "Pushover message sent! Retrying after ${PING_RETRY} seconds!"
+	sleep ${PING_RETRY}
+	echo "---------------------------------------------"
+	echo "-----------------RETRYING--------------------"
+	echo "---------------------------------------------"
+else
+	echo "Can't send message, retrying after ${PING_RETRY} seconds!"
+	sleep ${PING_RETRY}
+	echo "---------------------------------------------"
+	echo "-----------------RETRYING--------------------"
+	echo "---------------------------------------------"
+fi
